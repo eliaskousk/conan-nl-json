0d8990dfb35801af734a33fe5326617e05290db9
Elias Kouskoumvekakis <eliaskousk@gmail.com>
2017-02-13 04:14:18 +0200

    Update to 2.1.0

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conans.tools import download, unzip
 
 class NLJsonConan(ConanFile):
     name = "nl-json"
-    version = "2.0.9"
+    version = "2.1.0"
     url = "https://github.com/eliaskousk/conan-nl-json.git"
     license = "MIT"
     author = "Elias K."
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -11,7 +11,7 @@ class DefaultNameConan(ConanFile):
     version = "0.1"
     settings = "os", "compiler", "arch", "build_type"
     generators = "cmake"
-    requires = "nl-json/2.0.9@%s/%s" % (username, channel)
+    requires = "nl-json/2.1.0@%s/%s" % (username, channel)
 
     def build(self):
         cmake = CMake(self.settings)
